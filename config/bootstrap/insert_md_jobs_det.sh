133c6a061986a2f8bcda752ee59a6a825a258d0e
Neil Holcombe <njholcombe@gmail.com>
2025-09-29 10:27:53 -0700

    Update insert_md_jobs_det.sh

--- config/bootstrap/insert_md_jobs_det.sh
+++ config/bootstrap/insert_md_jobs_det.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
-
-# Insert expected_header
 aws dynamodb put-item \
   --table-name MD_JOBS_DET \
-  --item '{
-    "job_id": {"N": "1"},
-    "detail_value": {"S": "radio,mcc,mnc,lac,cid,lat,lon"},
-    "detail_name": {"S": "expected_header"}
-  }'
-
-# Insert param_name
-aws dynamodb put-item \
-  --table-name MD_JOBS_DET \
-  --item '{
-    "job_id": {"N": "1"},
-    "detail_value": {"S": "cell_towers.csv.gz"},
-    "detail_name": {"S": "file_name"}
-  }'
-
-# Insert raw_bucket
-aws dynamodb put-item \
-  --table-name MD_JOBS_DET \
-  --item '{
-    "job_id": {"N": "1"},
-    "detail_value": {"S": "/path/to/sql"},
-    "detail_name": {"S": "sql_file"}
-  }'
+  --item file://open_cellid_md_jobs_det.json
